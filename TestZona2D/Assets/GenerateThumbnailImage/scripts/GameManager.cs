7cc0ac10ed8fdcf53f62af3c2213430bec12f8dc
Dmitriy <nwstdio@gmail.com>
2023-08-25 16:40:56 +0300

    GenerateThumbnailImage
    - получение миниатюр изображений

---
+++ TestZona2D/Assets/GenerateThumbnailImage/scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GenerateThumbnailImage {
+    public class GameManager : MonoBehaviour {
+
+        [SerializeField] private Image _original, _result;// оригинал и уменьшенная картинки
+
+        private void Start() {
+            Texture2D texture = Utilities.CompressImage(_original.sprite.texture, .1f);// уменьшаем текстуру
+
+            File.WriteAllBytes(Application.persistentDataPath + "/thumbnail.isave", texture.EncodeToJPG());// сохраняем уменьшенную картинку
+
+            _result.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(.5f, .5f), 100f, 0, SpriteMeshType.FullRect);
+            }
+        }
+    }
---
+++ TestZona2D/Assets/GenerateThumbnailImage/scripts/Utilities.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace GenerateThumbnailImage {
+
+    public static class Utilities {
+
+        public static Texture2D CompressImage(Texture2D texture, float size = .5f) {
+            size = size.Clamp(.1f, .9f);
+
+            // размеры основной текстуры
+            int width = texture.width;
+            int height = texture.height;
+
+            // размеры уменьшеной текстуры
+            int new_width = (int)Mathf.Ceil(width * size);
+            int new_height = (int)Mathf.Ceil(height * size);
+
+            var colors = new Color[new_height, new_width];// сумма цветов для пикселя
+            var quantities = new int[new_height, new_width];// количество пикселей входящих в пиксель новой текстуры
+
+            // пройдемся по пикселям основной текстуры
+            for (int y = 0; y < height; y++) {
+                int yy = (int)Mathf.Floor(y * size);
+
+                for (int x = 0; x < width; x++) {
+                    int xx = (int)Mathf.Floor(x * size);
+
+                    colors[yy, xx] += texture.GetPixel(x, y);
+
+                    quantities[yy, xx]++;
+                    }
+                }
+
+            var newImage = new Texture2D(new_width, new_height, TextureFormat.ARGB32, false) {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp
+                };// создадим уменьшенную текстуру
+
+            // пройдемся по уменьшенной текстуре
+            for (int y = 0; y < new_height; y++) {
+                for (int x = 0; x < new_width; x++)
+                    newImage.SetPixel(x, y, colors[y, x] / quantities[y, x]);
+                }
+
+            newImage.Apply();
+
+            return newImage;
+            }
+
+        public static float Clamp(this float value, float min, float max) => Mathf.Clamp(value, min, max);
+
+        }
+    }
