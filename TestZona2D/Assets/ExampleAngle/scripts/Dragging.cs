dd8ff6cf0eacea7b712f099db6f9bfaf125dc59e
Dmitriy <nwstdio@gmail.com>
2023-08-28 14:58:00 +0300

    ExampleAngle
    - пример расчета угла от направления взгляда до цели

---
+++ TestZona2D/Assets/ExampleAngle/scripts/Dragging.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ExampleAngle {
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class Dragging : MonoBehaviour { // перетаскивание обьекта
+
+        private Camera _camera;// камера
+        private Vector3 _dragOffset;// величина смещения
+
+        private void Start() {
+            _camera = Camera.main;
+            }
+
+        private void OnMouseDown() {
+            _dragOffset = transform.position - _camera.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+        private void OnMouseDrag() {
+            transform.position = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f)) + _dragOffset;
+
+            Main.Instance.CalculateAngle();
+            }
+
+        }
+    }
